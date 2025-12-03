de594a795dafa7bfc0030a7dc17883c014174414
Antonio Neto <netosilva321123@gmail.com>
2025-12-03 12:52:59 +0000

    2 horas

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -1,52 +1,51 @@
-class Fone:
-    def __init__(self, id: str, number: str):
+class Telefone:
+    def __init__(self, id: str, numero: str):
         self.__id: str = id
-        self.__number: str = number
+        self.__numero: str = numero
 
     def isValid(self):
-        em = "013456789()."
-        return all(e in em for e in self.__number)
+        permitidos = "013456789()."
+        return all(ch in permitidos for ch in self.__numero)
 
     def getId(self):
         return self.__id
 
-    def getNumber(self):
-        return self.__number
+    def getNumero(self):
+        return self.__numero
     
     def __str__(self):
-        return f"{self.__id}:{self.__number}"
-
+        return f"{self.__id}:{self.__numero}"
+    
 class Contato:
     def __init__(self, nome: str):
         self.__nome: str = nome
-        self.__fone: list = []
+        self.__telefones: list = []
         self.__favorited: bool = False
 
-    def addfone(self, id: str, number: str):
-        fone = Fone(id, number)
-        if fone.isValid():
-            self.__fone.append(fone)
+    def addTelefone(self, id: str, numero: str):
+        tel = Telefone(id, numero)
+        if tel.isValid():
+            self.__telefones.append(tel)
             return
-        print("fail: invalid number")
+        print("fail: número inválido")
 
-    def rmFone(self, index: int):
+    def rmTelefone(self, index: int):
         try:
-            self.__fone.pop(index)
+            self.__telefones.pop(index)
         except:
-            print("fail: index invalido")
+            print("fail: índice inválido")
 
     def toggleFavorited(self):
         self.__favorited = not self.__favorited
 
-    def getFones(self):
-        return self.__fone
+    def getTelefones(self):
+        return self.__telefones
     def __str__(self):
         frag = "@" if self.__favorited else "-"
-        return f"{frag} {self.__nome} [" + ", ".join(str(e) for e in self.__fone)+"]"
+        return f"{frag} {self.__nome} [" + ", ".join(str(e) for e in self.__telefones)+"]"
 
     def isFavorited(self):
         return self.__favorited
-
 def main():
     contato = Contato("")
 
@@ -60,6 +59,23 @@ def main():
 
         elif args[0] == "show":
             print(contato)
+
+        elif args[0] == "init":
+            contato = Contato(args[1])
+
+        elif args[0] == "add":
+            contato.addTelefone(args[1], args[2])
+
+        elif args[0] == "rm":
+            contato.rmTelefone(int(args[1]))
+
+        elif args[0] == "tfav":
+            contato.toggleFavorited()
+        else:
+            print("fail:comando invalido")
+main()
+
+
 
 
     
